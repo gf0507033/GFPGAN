0241d943fcb9ea9bd22b6ad2387d0bab495c878a
Grigory Feldman <fgb631@yandex.ru>
2024-07-19 19:07:45 +0300

    Update utils.py

--- gfpgan/utils.py
+++ gfpgan/utils.py
@@ -84,7 +84,7 @@ class GFPGANer():
             save_ext='png',
             use_parse=True,
             device=self.device,
-            model_rootpath=model_rootpath)
+            model_rootpath=facehelper_path)
 
         if model_path.startswith('https://'):
             model_path = load_file_from_url(
